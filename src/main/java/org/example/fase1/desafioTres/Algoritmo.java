7305822924844188c210863187729f0f5f8be8d7
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-09-19 14:46:13 -0300

    Commit nova funcionalidade criada

--- src/main/java/org/example/fase1/desafioTres/Algoritmo.java
+++ src/main/java/org/example/fase1/desafioTres/Algoritmo.java
@@ -1,4 +1,36 @@
 package org.example.fase1.desafioTres;
 
+import java.util.Scanner;
+
 public class Algoritmo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean entrada = false;
+
+        while (!entrada) {
+            try {
+                System.out.print("Digite um valor inteiro entre 0 e 13: ");
+                int n = scanner.nextInt();
+
+                if (n >= 0 && n < 13) {
+                    calculeFatorial(n);
+                    entrada = true;
+                } else {
+                    System.out.println("O valor de informado está fora do intervalo permitido.");
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Entrada inválida. Certifique-se de digitar um número inteiro.");
+            }
+        }
+    }
+
+    public static void calculeFatorial(int n) {
+        int fatorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+        System.out.println("O fatorial de " + n + " é: " + fatorial);
+    }
 }
+
